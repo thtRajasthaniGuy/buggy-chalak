249eae676fa9ba8bf19f149f06c112f00273429c
Amitverma43 <amyvee43@gmail.com>
2023-09-04 20:31:43 +0530

    work section

--- js/worksection.js
+++ js/worksection.js
@@ -1,4 +1,4 @@
-function changeWorkView() {
+function changeWorkView(id) {
 
    const riders_section = document.getElementById("riders_section");
    const riders_section1 = document.getElementById("riders_section1");
@@ -6,34 +6,35 @@ function changeWorkView() {
    const captains_section = document.getElementById("captains_section");
    const captains_section1 = document.getElementById("captains_section1");
    const captains_section2 = document.getElementById("captains_section2");
+   if (id !== 'riders_btn') {
+      riders_section.style.display = "None";
+      riders_section1.style.display = "None";
+      riders_section2.style.display = "None";
+      captains_section.style.display = "block";
+      captains_section1.style.display = "block";
+      captains_section2.style.display = "block";
+   } else  {
+      riders_section.style.display = "block";
+      riders_section1.style.display = "block";
+      riders_section2.style.display = "block";
+      captains_section.style.display = "None";
+      captains_section1.style.display = "None";
+      captains_section2.style.display = "None";
+   }
+}
 
-   riders_section.style.display = riders_section.style.display == "block" ? "None" : "block";
-   riders_section1.style.display = riders_section1.style.display == "block" ? "None" : "block";
-   riders_section2.style.display = riders_section2.style.display == "block" ? "None" : "block";
-   captains_section.style.display =  captains_section.style.display == "block" ? "None" : "block";
-   captains_section1.style.display =  captains_section1.style.display == "block" ? "None" : "block";
-   captains_section2.style.display =  captains_section2.style.display == "block" ? "None" : "block";
-  
+window.onload = function () {
 
- }
-
- 
- window.onload = function () {
- 
-    const riders_section = document.getElementById("riders_section");
-    const riders_section1 = document.getElementById("riders_section1");
-    const riders_section2 = document.getElementById("riders_section2");
-    const captains_section = document.getElementById("captains_section");
-    const captains_section1 = document.getElementById("captains_section1");
-    const captains_section2 = document.getElementById("captains_section2");
-
-
-    riders_section.style.display = "block";
-    riders_section1.style.display = "block";
-    riders_section2.style.display = "block";
-    captains_section.style.display = "None";
-    captains_section1.style.display = "None";
-    captains_section2.style.display = "None";
-   
-    
- }
+   const riders_section = document.getElementById("riders_section");
+   const riders_section1 = document.getElementById("riders_section1");
+   const riders_section2 = document.getElementById("riders_section2");
+   const captains_section = document.getElementById("captains_section");
+   const captains_section1 = document.getElementById("captains_section1");
+   const captains_section2 = document.getElementById("captains_section2");
+   riders_section.style.display = "block";
+   riders_section1.style.display = "block";
+   riders_section2.style.display = "block";
+   captains_section.style.display = "None";
+   captains_section1.style.display = "None";
+   captains_section2.style.display = "None";
+}
